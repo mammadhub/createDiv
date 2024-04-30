fb382d9d52a1a5c44ee367e63916a6ff508e218e
mammad <memmed2512@gmail.com>
2024-04-30 17:29:16 +0400

    createDiv

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 function createDiv() {
-    const height = document.querySelector(".height").value;
-    const width = document.querySelector(".width").value;
+    const height = parseFloat(document.querySelector(".height").value);
+    const width = parseFloat(document.querySelector(".width").value);
     const unit = document.querySelector(".unit").value;
     const div = document.createElement("div");
-    div.style.width = width + unit;
-    div.style.height = height + unit;
-    div.style.backgroundColor = "blue";
+    if (typeof width === "number" && typeof height === "number") {
+        if (width < 0 || height < 0) {
+            alert ("dogru eded daxil ed");
+        }
+        else if(width >= 0 || height >= 0) {
+            div.style.width = width + unit;
+            div.style.height = height + unit;
+            div.style.backgroundColor = "blue";
 
-    const container = document.querySelector(".container");
-    const oldDiv = container.querySelector(".created-div");
+            const container = document.querySelector(".container");
+            const oldDiv = container.querySelector(".created-div");
+
+            if (oldDiv) {
+                container.removeChild(oldDiv);
+            }
+
+            div.classList.add("created-div");
+            container.appendChild(div);
+        }
+        else {
+            alert ("dogru eded daxil ed");
+        }
     
-    if (oldDiv) {
-        container.removeChild(oldDiv);
     }
 
-    div.classList.add("created-div");
-    container.appendChild(div);
 }
+
